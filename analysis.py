ce1f2572d74441f084098363ff74c240d798c64b
Diyar Oktay <diyar.oktay@outlook.com>
2022-08-15 15:45:17 +0200

    Add documentation

--- analysis.py
+++ analysis.py
@@ -8,19 +8,37 @@ TYRANU = 1
 values = (2,3,4,5,6,7,8,9,10,'J','K','Q','A')
 
 def new_deck():
-    """This function returns a new, shuffled deck"""
+    """Returns a full shuffled deck of cards
+    
+    Returns
+        :deck: A deck of cards with len 52 where the items are in the domain
+         2 to 'A'
+    """
     deck = [(v) for v in values for _ in range(4)]
     shuffle(deck)
     return deck
 
 def new_counter():
-    """a counter is a device that marks how many of each
-    card has been revealed. initially there are 4 of each
-    card"""
+    """A counter indicates how many of each card are left in the deck.
+    
+    Returns
+        :counter: A counter initialized with 4 for each type of card
+    """
     return [4 for _ in values]
 
 def best_choice(counter, head):
-    """"""
+    """
+    Given a counter and a revealed card, return whether it is better to choose
+    TYRANU or EVAVU. Also mutates the counter by subtracting 1 from the revealed
+    card.
+
+    Parameters
+        :counter: The counter, see function new_counter
+        :head:    The card on top 
+    Returns
+        :choice: The best choice in this scenario, which is one of the constants
+        TYRANU or EVAVU. In case of equal odds either of them is returned.
+    """
     i = values.index(head)
     # remove the card from the counter
     counter[i] -= 1
@@ -59,6 +77,7 @@ if __name__ == "__main__":
     stats_only = False
     stats = None
 
+    # Parse command line arguments
     try:
         filename = sys.argv[1]
     except IndexError:
@@ -70,6 +89,8 @@ if __name__ == "__main__":
     except IndexError:
         pass
 
+
+    # Print stats and exit
     if stats_only:
         try:
             stats = np.load(filename)
@@ -83,6 +104,8 @@ if __name__ == "__main__":
             print(f"file {filename} not found")
             sys.exit(1)
 
+    # Run simulations and save them to the specified file when a keyboard
+    # interrupt is registered
     if not stats_only:
         try:
             stats = np.load(filename)
